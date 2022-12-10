b067b34c51f98061a2afd1e562e784e54f66ae58
Youssef ELASERY <joseph.elasery@gmail.com>
2022-12-10 12:27:08 +0100

    update the dashborad app

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,10 +1,9 @@
-import pandas as pd  # pip install pandas openpyxl
-import plotly.express as px  # pip install plotly-express
-import streamlit as st  # pip install streamlit
+import pandas as pd
+import plotly.express as px
+import streamlit as st
 import pymongo
 
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
-# Initialize connection.
 
 def init_connection():
     return pymongo.MongoClient(**st.secrets["mongo"])
