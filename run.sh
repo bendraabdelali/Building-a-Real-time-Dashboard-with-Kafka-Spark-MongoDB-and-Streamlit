ed02c771f125fd711d5fc1700299baa18e02e491
bendraabdelali <abdelalibendra123@gmail.com>
2022-12-14 13:36:50 +0100

    Revert "Adding documentation to this file helps users understand and use the project more effectively."
    This reverts commit bf4e6fa7639b912a45028ae5082dc3dcd4c9eac2.

--- run.sh
+++ run.sh
@@ -1,21 +1,25 @@
 #docker-compose up -d 
 ConatinerKafka="kafka"
 ConatinerSpark="spark"
-# Create an analysis database on MongoDB by accessing a MongoDB admin using this URL: localhost:8081 and give 'mexpress' as the username and password
+# Create Analysis database on Mongodb
 
 #Create Topic 
-docker exec -u 0 kafka bash -c "kafka-topics.sh --create --bootstrap-server kafka:9092 --partitions 1 --replication-factor 1 --topic topicA"
+cmd1="kafka-topics.sh --create --bootstrap-server kafka:9092 --partitions 1 --replication-factor 1 --topic topicA"
 
 # Start Stream App
-docker exec -u 0 kafka bash -c "spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0,org.mongodb.spark:mongo-spark-connector:10.0.0 /data/StreamAggCountry.py"
+cmd2="spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0,org.mongodb.spark:mongo-spark-connector:10.0.0 /data/StreamAggCountry.py"
 
-docker exec -u 0 kafka bash -c "spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0,org.mongodb.spark:mongo-spark-connector:10.0.0 /data/StreamAggProuduct.py"
+cmd3="spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0,org.mongodb.spark:mongo-spark-connector:10.0.0 /data/StreamAggProuduct.py"
 
-docker exec -u 0 kafka bash -c "spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0,org.mongodb.spark:mongo-spark-connector:10.0.0  /data/StreamAggGender.py "
+cmd4="spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0,org.mongodb.spark:mongo-spark-connector:10.0.0  /data/StreamAggGender.py "
 
-# start Ecommerce data generator
-cd Ecommerce\ data\ generator
-python producer.py
+#uncomment app in docker-cmpose and rerun docker-compose to start the app that will sent the data into kafka evry 10 seconds 
 
-#run the Dashbord 
-python -m streamlit run Dashboard/dashboard.py
+
+docker exec -u 0 $ConatinerKafka bash -c "$cmd1 && $cmd2"
+
+docker exec -u 0 $ConatinerSpark bash -c "$cmd3"
+
+docker exec -u 0 $ConatinerSpark bash -c "$cmd4"
+
+#run the Dashbord python -m streamlit run .\2_??_Dash.py
